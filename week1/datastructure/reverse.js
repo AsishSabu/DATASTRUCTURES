6f583407571b49a3b3ed97cdfb4864fb7ebec7fe
Asish <ashisish1858@gamil.com>
2024-01-10 13:15:25 +0530

    reverse of linked list

---
+++ week1/datastructure/reverse.js
@@ -0,0 +1,56 @@
+class node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+class linkedList {
+  constructor() {
+    this.head = null;
+  }
+  display() {
+    if (!this.head) {
+      console.log("empty linked list");
+      return;
+    }
+    let current = this.head;
+    let result = "";
+    while (current) {
+      result += ` ${current.data} `;
+      current = current.next;
+    }
+    console.log(result);
+  }
+  append(data) {
+    const newNode = new node(data);
+    if (!this.head) {
+      this.head = newNode;
+      return;
+    }
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+    current.next = newNode;
+  }
+  reverse() {
+    let previous = null;
+    let current = this.head;
+    let nextnode= null;
+    while (current) {
+      let nextnode = current.next;
+      current.next = previous;
+      previous = current
+      current=nextnode
+   
+    }
+   this.head = previous
+  }
+    
+}
+const list = new linkedList();
+list.append(12)
+list.append(10)
+list.append(7)
+list.reverse()
+list.display();
